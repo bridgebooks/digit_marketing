ae387ef335ac0005abb25719a59c314e0c63b55b
Patrick Foh <patrickfoh@gmail.com>
2017-05-29 23:39:46 +0100

    Updated routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express').Router()
+const pricingController = require('../controllers/pricing')
 
 router.use((req, res, next) => {
     next()
@@ -17,8 +18,19 @@ router.get('/', (req, res) => {
 router.get('/features', (req, res) => {
     res.render('features', {
         page_title: "Feature Tour",
-        page_description: "DigIT Features",
+        page_description: "DigIT is a simple online bookkeep and HR software for busy teams, agencies, fast-growing startups and small businesses everywhere.",
         page_author: "First Renaniasance Assoicates"
     })
 })
+
+router.get('/pricing', pricingController.index)
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        page_title: "Contact Us",
+        page_description: "Get in touch with us",
+        page_author: "First Renaniasance Assoicates"
+    })
+})
+
 module.exports = router
